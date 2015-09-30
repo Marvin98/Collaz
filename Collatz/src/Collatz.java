ae0682cd0e5105170b2a1798c8f651f545f93b02
Buchert.Marvin <marvin.buchert@web.de>
2015-09-30 17:03:41 +0200

    1

---
+++ Collatz/src/Collatz.java
@@ -0,0 +1,18 @@
+
+public class Collatz {
+
+	public static void main(String[] args) {
+		int startzahl = 765;
+	    while(startzahl >1)
+	    	if(startzahl%2==0){
+	    		startzahl=startzahl/2;
+	    	}
+	    
+	    
+	    	else(startzahl=(startzahl*3)+1;}
+
+	if(startzahl>1){System.out.print(startzahl+", ");}
+	else{System.out.print(startzahl);}
+	}
+
+}
